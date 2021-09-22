bc519c955bd3c1a63c08325d471088f277849a34
Aditi <bokil.aditi@gmail.com>
2021-09-22 15:50:22 +0530

    Second Commit

--- Customer.java
+++ Customer.java
@@ -2,5 +2,7 @@ public class Customer {
 
 	public static void main(String args[]){
 		System.out.println("Hello from Git customer");
+		System.out.println("Hello from Git customer again");
+		
 	}
 }
